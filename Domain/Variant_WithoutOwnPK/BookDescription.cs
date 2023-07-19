bcfb9fcdedae83a046523624b48be5bbf2e5797d
gonzobard777 <gonzo.bard@dwfe.ru>
2023-07-19 11:38:44 +0300

    .

--- Domain/Variant_WithoutOwnPK/BookDescription.cs
+++ Domain/Variant_WithoutOwnPK/BookDescription.cs
@@ -1,10 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Domain.Variant_WithoutOwnPK;
+﻿namespace Domain.Variant_WithoutOwnPK;
 
 public class BookDescription : BookDescriptionBase
 {
-    [Key]
     public int BookId { get; set; }
     public Book Book { get; set; }
 }
